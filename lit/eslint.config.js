88761f8bd5c2b058198af3c7e2ca27552d837017
Gilbert563307 <94962686+Gilbert563307@users.noreply.github.com>
2025-10-13 21:42:09 +0200

    eslint config updated issues ffx, added reame about atoms organsims etc

--- lit/eslint.config.js
+++ lit/eslint.config.js
@@ -1,7 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+    },
+    plugins: { js },
+    rules: {
+      ...js.configs.all.rules,
+      'sort-keys': 'off',
+      'sort-imports': 'off',
+      // 'capitalized-comments': 'warn',
+      // 'no-inline-comments': 'warn',
+      'one-var': ['error', 'never'],
+      'no-warning-comments': 'off',
+      'no-console': 'warn',
+    }
+
+  },
 ]);
---
+++ lit/src/utils.js
@@ -0,0 +1,8 @@
+var h = 7; // height
+var w = 5; // width
+
+function a(h, w) {
+  return h * w * 10;
+}
+
+console.log(h + ' * ' + w + ' = ' + a(h, w));
